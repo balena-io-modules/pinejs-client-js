20f709ad10e056b6b7e11324a792d24afe862c6b
Pagan Gazzard <page@resin.io>
2017-09-30 18:26:22 -0700

    Allow passing in the promise types in order to get a correctly typed promise returned
    Change-type: patch

--- request.d.ts
+++ request.d.ts
@@ -1,7 +1,8 @@
 import * as PinejsClientCoreFactory from './core'
 import * as request from 'request'
+import * as Promise from 'bluebird'
 
-export = class PinejsClientRequest extends PinejsClientCoreFactory.PinejsClientCore<PinejsClientRequest> {
+export = class PinejsClientRequest extends PinejsClientCoreFactory.PinejsClientCore<PinejsClientRequest, Promise<{}>, Promise<number | PinejsClientCoreFactory.AnyObject | PinejsClientCoreFactory.AnyObject[]>> {
 	constructor(
 		params: string | PinejsClientCoreFactory.Params,
 		backendParams?: {
